fd58a2579c7422c794a12675337151489b08e0a7
IntegralFox <michael.welborn0775@gmail.com>
2015-02-08 14:39:52 -0600

    Convert wrapper main to use files instead of stdin/stdout

--- src/minimize.cpp
+++ src/minimize.cpp
@@ -1,11 +1,23 @@
 /* Main file for user interaction */
 #include <iostream>
 #include <fstream>
+#include <memory>
 #include "dfa.hpp"
 
 int main(int argc, char** argv) {
-	DFA automaton {std::cin};
-	std::cout << automaton;
+	if (argc == 1) {
+		std::cerr << "No DFA file specified." << std::endl;
+		return 0;
+	}
+
+	// Acquire the files
+	std::string filename {argv[1]};
+	std::ifstream dfaFile {filename};
+	std::ofstream dotFile {filename + ".dot"}, minDotFile {filename + "-min.dot"};
+
+	// Create a DFA object and minimize
+	DFA automaton {dfaFile};
+	dotFile << automaton;
 	automaton.minimize();
-	std::cout << automaton;
+	minDotFile << automaton;
 }
