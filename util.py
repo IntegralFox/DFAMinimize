e7813095f0e500ab377cc11e372542b87dee1d9a
IntegralFox <michael.welborn0775@gmail.com>
2015-02-07 18:48:10 -0600

    Static members. Ahhhhh!

--- util.py
+++ util.py
@@ -4,44 +4,41 @@ Utility functions for minimization.
 
 
 class State:
-	final = False
-	start = False
-	transition = {}
+	def __init__(self):
+		self.final = False
+		self.start = False
+		self.transition = {}
 
 
-def readDFA(dfaFile):
-	dfa = {}
-	dfa['state'] = {}
+class DFA:
+	def __init__(self, dfaFile):
+		self.state = {}
+		self.key = []
 
-	# Read the first line (state names) and create state objects for each
-	for key in dfaFile.readline().strip().split():
-		dfa['state'][key] = State()
+		# Read the first line (state names) and create state objects for each
+		for key in dfaFile.readline().strip().split():
+			self.key.append(key)
+			self.state[key] = State()
 
-	# Read in the alphabet
-	dfa['alphabet'] = dfaFile.readline().strip().split()
+		# Read in the alphabet
+		self.alphabet = dfaFile.readline().strip().split()
 
-	# Set the start state flag
-	startKey = dfaFile.readline().strip()
-	dfa['state'][startKey].start = True
-	dfa['start'] = startKey
+		# Set the start state flag
+		self.start = dfaFile.readline().strip()
+		self.state[self.start].start = True
 
-	# Read in the final state names and set the appropriate flags
-	dfa['final'] = dfaFile.readline().strip().split()
-	for key in dfa['final']:
-		dfa['state'][key].final = True
+		# Read in the final state names and set the appropriate flags
+		self.final = dfaFile.readline().strip().split()
+		for key in self.final:
+			self.state[key].final = True
 
-	# Set the transitions in each state based on the rest of the rules
-	for line in dfaFile:
-		key, symbol, transitionKey = line.strip().split()
-		dfa['state'][key].transition[symbol] = transitionKey
-
-	return dfa
+		# Set the transitions in each state based on the rest of the rules
+		for line in dfaFile:
+			key, symbol, transitionKey = line.strip().split()
+			self.state[key].transition[symbol] = transitionKey
 
 
 # Returns the logical XOR of two booleans since this operator does not
 # Exist in python
 def xor(op0, op1):
-	if op0 != op1:
-		return True
-	else:
-		return False
+	return op0 != op1
