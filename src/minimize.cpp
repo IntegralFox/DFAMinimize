15a039445ed334d4ceb0fe8c7039e6b2e8dcf964
IntegralFox <michael.welborn0775@gmail.com>
2015-02-09 21:03:36 -0600

    Improve error handling some more

--- src/minimize.cpp
+++ src/minimize.cpp
@@ -15,14 +15,17 @@ int main(int argc, char** argv) {
 		// Open the DFA and create the DFA object
 		std::string filename {argv[1]};
 		std::ifstream dfaFile {filename};
+		if (!dfaFile.is_open()) throw std::runtime_error {"Could not open file " + filename};
 		DFA automaton {dfaFile};
 
 		// Open the output file and output the original DFA in the dot language
 		std::ofstream dotFile {filename + ".dot"};
+		if (!dotFile.is_open()) throw std::runtime_error {"Could not open file " + filename + ".dot"};
 		dotFile << automaton;
 
 		// Open the output file and output the minimized DFA
 		std::ofstream minDotFile {filename + "-min.dot"};
+		if (!minDotFile.is_open()) throw std::runtime_error {"Could not open file " + filename + "-min.dot"};
 		automaton.minimize();
 		minDotFile << automaton;
 	} catch (std::runtime_error e) {
